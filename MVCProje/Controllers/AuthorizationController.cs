0337a2c893095d6add9791b525a03f87a9587910
Özgür <carozgur@yahoo.com>
2021-07-06 00:14:36 +0300

    micro edit

--- MVCProje/Controllers/AuthorizationController.cs
+++ MVCProje/Controllers/AuthorizationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,44 @@ namespace MVCProje.Controllers
     public class AuthorizationController : Controller
     {
         AdminManager adm = new AdminManager(new EfAdminDal());
+
         // GET: Authorization
         public ActionResult Index()
         {
             var adminvalues = adm.GetAdminList();
             return View(adminvalues);
+        }
+
+
+
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdmin(Admin p)
+        {
+            adm.AdminAdd(p);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+
+            var avalue = adm.GetByID(id);
+            return View(avalue);
+        }
+        [HttpPost]
+        public ActionResult EditAdmin(Admin p)
+        {
+            adm.AdminUpdate(p);
+            return RedirectToAction("Index");
         }
     }
 }
